1bd9c4aff5b5260748462c079fed223569b5627f
Wojciech Trocki <wtrocki@redhat.com>
2019-06-27 14:59:42 +0100

    fix: Linting problems

--- src/app/pages/update-item/update-item.page.ts
+++ src/app/pages/update-item/update-item.page.ts
@@ -60,7 +60,7 @@ export class UpdateItemPage extends OfflineNotifier implements OnInit {
     }).catch((error) => {
       this.handleOfflineMutation(error);
       if (error.networkError && error.networkError.localConflict) {
-        // Developers can merge data, but in this case we are simply 
+        // Developers can merge data, but in this case we are simply
         // providing fresh one.
         this.presentToast('Item you are trying to edit was changed on server. Please review your changes');
         this.item = error.networkError.base;
